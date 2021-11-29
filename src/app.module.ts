aec930f8a7c4c2dcdfa976c5c2bc0c3c0e1c6e25
Tungky Adi Pradana <tungky.adi@uii.ac.id>
2021-11-29 09:43:09 +0700

    Add new file for tasks management service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,5 @@ import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+
+  @Delete()
+  deleteAllTasks(): string {
+    return this.tasksService.deleteAllTasks();
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string): string {
+    return this.tasksService.deleteTaskById(id);
+  }
+}
