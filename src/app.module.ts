1bef56a860a04082780c33fbba688044803a17bc
tungkyap <adi.pradana95@gmail.com>
2021-12-23 15:38:06 +0700

    Solve merge conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,7 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-=======
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [TasksModule],
->>>>>>> 6274d7e0a8ef5906982d64ffa1fe1a53af64db9c
 })
 export class AppModule {}
