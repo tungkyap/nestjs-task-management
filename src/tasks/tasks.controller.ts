2c24e0ff0183cd589103ebeb56bfe3512945186f
tungkyap <adi.pradana95@gmail.com>
2021-12-29 11:18:10 +0700

    Add AuthGuard and import AuthModule to tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { FilterTasksDto } from './dto/filter-tasks.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TasksRepository } from './tasks.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
   imports: [
-    TypeOrmModule.forFeature([TasksRepository])
+    TypeOrmModule.forFeature([TasksRepository]),
+    AuthModule
   ]
 })
 export class TasksModule {}
